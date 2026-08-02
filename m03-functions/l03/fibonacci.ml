fb14f4dba68a9dda8d056a510572a4e7c7b40516
Jonathan E <207332460+jonathan-16bit@users.noreply.github.com>
2026-08-02 12:43:03 +0530

    feat: add recursive fibonacci fn

---
+++ m03-functions/l03/fibonacci.ml
@@ -0,0 +1,7 @@
+(* Stack overflow for negative numbers *)
+let rec fib n = 
+  if n = 0 || n = 1 then n
+  else fib (n - 1) + fib (n - 2)
+
+(* Print *)
+let () = print_endline (string_of_int (fib 32))
